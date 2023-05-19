4837b889a68650763174b4cf7b32adec8f288c67
idkuri <acaoy2@gmail.com>
2023-05-19 07:49:24 -0400

    updated script

--- update_script.sh
+++ update_script.sh
@@ -2,10 +2,13 @@
 
 cd "C:\Users\Kuri\Desktop\Portfolio"
 npm run build
+git fetch --recurse-submodules
+git submodule update --remote
 cp -R build/* idkuri.github.io/
 cd idkuri.github.io
 git add --all
 git commit -m "Updated Files with bash script"
 git push origin
 cd ..
+read "Hi"
 echo "Script execution completed!"
