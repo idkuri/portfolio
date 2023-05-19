20d3769d2c041dc70eab1b95d977b718c744bbf8
unknown <acaoy2@gmail.com>
2023-05-19 05:43:21 -0400

    changed script

--- update_script.sh
+++ update_script.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
 
 cd "C:\Users\Kuri\Desktop\Portfolio"
-# Step 1: Run npm run build
 npm run build
-
-# Step 2: Copy files using robocopy (Windows)
 cp -R build/* idkuri.github.io/
-
-# Step 3: Enter the submodule directory
 cd idkuri.github.io
-
-# Step 4: Stage all changes in the submodule
 git add --all
-
-# Step 5: Commit the changes with a specified message
 git commit -m "Updated Files with bash script"
-
-# Step 6: Push the changes to the remote repository
 git push origin
-
-# Step 7: Go back to the main repository's root directory
 cd ..
-
-read "hi"
 echo "Script execution completed!"
