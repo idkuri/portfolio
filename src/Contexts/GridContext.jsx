5b2dd4554c49d967c9188e4beb03ef74cdfc2763
Andrew Cao <acaoy2@gmail.com>
2025-10-01 05:14:15 -0400

    update submodule

--- src/Contexts/GridContext.jsx
+++ src/Contexts/GridContext.jsx
@@ -43,6 +43,7 @@ export const GridProvider = ({ children }) => {
 
     useEffect(() => {
         resizeHandler();
+        setToggled(true)
     }, [])
 
     useEffect(() => {
--- update_script.sh
+++ update_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd "C:\Users\Kuri\Desktop\Portfolio\portfolio"
-npm run build
+# npm run build
 echo "Updating Submodules"
 git fetch --recurse-submodules
 git submodule update --remote
